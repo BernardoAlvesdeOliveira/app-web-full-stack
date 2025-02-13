691c56ffff47f98ffc1803b7d7db2b922d2768ae
Bernardo <balvesdeoliveira3@gmail.com>
2025-02-13 11:04:00 -0300

    add CORS and add connection to route product_bp

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from routes.product_routes import product_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/test", methods=['GET'])
 def test():
     return "Rota de teste funcionando!"
+
+app.register_blueprint(product_bp, url_prefix="/api")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
